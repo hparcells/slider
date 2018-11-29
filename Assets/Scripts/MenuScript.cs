5c996fe2c2d46b4f204e22a8bbd26f40a0a739cc
Hunter Parcells <hparcells@icloud.com>
2018-11-29 14:57:03 -0500

    Menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+	void Update () {
+        if(Input.GetKey(KeyCode.RightArrow)) {
+            Application.Quit();
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow)) {
+            Initiate.Fade("Level1", Color.white, 2);
+        }
+    }
+}
